6261f88c19f14104099ba3a30111c35275c12a9c
John Hubbard <jhubbard@nvidia.com>
2014-10-17 22:14:52 -0700

    Verilog 0764 hw4: save point: year table transformed

---
+++ ucsc/verilog_0764/hw4/cal.v
@@ -0,0 +1,33 @@
+// File: cal.v
+// John Hubbard, 17 Oct 2014
+// hw4 assignment for Verilog 0764 class
+//
+// Steps to run in ModelSim (Linux this time):
+// cd $HOME/git_wa/classes/ucsc/verilog_0764/hw4/modelsim
+// vlib work
+// vlog -sv ../cal.v
+// vsim work.cal_tb
+// add wave *
+// run 100 ns
+//
+
+`timescale 1ns/1ns
+module cal_tb;
+    parameter DEPTH = 279; // 2033 - 1755 + 1
+    parameter WIDTH = 20;
+    integer year;
+
+    reg [WIDTH - 1:0] yearTable[DEPTH - 1:0];
+
+    initial
+    begin
+        readmemh("year_table_transformed.txt", yearTable);
+        #10 $display("year_table_transformed.txt:\n");
+
+        for (year = 0; year < DEPTH; year++)
+        begin
+           // $display("year: %d, key: %h\n", yearTable[
+        end
+
+    end
+endmodule
---
+++ ucsc/verilog_0764/hw4/transform_data.c
@@ -0,0 +1,287 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("%x%1x\n", 1755, 3);
+    printf("%x%1x\n", 1756, 11);
+    printf("%x%1x\n", 1757, 6);
+    printf("%x%1x\n", 1758, 7);
+    printf("%x%1x\n", 1759, 1); // A == 1
+    printf("%x%1x\n", 1760, 9);
+    printf("%x%1x\n", 1761, 4);
+    printf("%x%1x\n", 1762, 5);
+    printf("%x%1x\n", 1763, 6);
+    printf("%x%1x\n", 1764, 14);
+    printf("%x%1x\n", 1765, 2); // B == 2;
+    printf("%x%1x\n", 1766, 3);
+    printf("%x%1x\n", 1767, 4);
+    printf("%x%1x\n", 1768, 12);
+    printf("%x%1x\n", 1769, 7);
+    printf("%x%1x\n", 1770, 1); // A == 1
+    printf("%x%1x\n", 1771, 2); // B == 2;
+    printf("%x%1x\n", 1772, 10);
+    printf("%x%1x\n", 1773, 5);
+    printf("%x%1x\n", 1774, 6);
+    printf("%x%1x\n", 1775, 7);
+    printf("%x%1x\n", 1776, 8);
+    printf("%x%1x\n", 1777, 3);
+    printf("%x%1x\n", 1778, 4);
+    printf("%x%1x\n", 1779, 5);
+    printf("%x%1x\n", 1780, 13);
+    printf("%x%1x\n", 1781, 1); // A == 1
+    printf("%x%1x\n", 1782, 2); // B == 2;
+    printf("%x%1x\n", 1783, 3);
+    printf("%x%1x\n", 1784, 11);
+    printf("%x%1x\n", 1785, 6);
+    printf("%x%1x\n", 1786, 7);
+    printf("%x%1x\n", 1787, 1); // A == 1
+    printf("%x%1x\n", 1788, 9);
+    printf("%x%1x\n", 1789, 4);
+    printf("%x%1x\n", 1790, 5);
+    printf("%x%1x\n", 1791, 6);
+    printf("%x%1x\n", 1792, 14);
+    printf("%x%1x\n", 1793, 2); // B == 2;
+    printf("%x%1x\n", 1794, 3);
+    printf("%x%1x\n", 1795, 4);
+    printf("%x%1x\n", 1796, 11);
+    printf("%x%1x\n", 1797, 7);
+    printf("%x%1x\n", 1798, 1); // A == 1
+    printf("%x%1x\n", 1799, 2); // B == 2;
+    printf("%x%1x\n", 1800, 3);
+    printf("%x%1x\n", 1801, 4);
+    printf("%x%1x\n", 1802, 5);
+    printf("%x%1x\n", 1803, 6);
+    printf("%x%1x\n", 1804, 14);
+    printf("%x%1x\n", 1805, 2); // B == 2;
+    printf("%x%1x\n", 1806, 3);
+    printf("%x%1x\n", 1807, 4);
+    printf("%x%1x\n", 1808, 12);
+    printf("%x%1x\n", 1809, 7);
+    printf("%x%1x\n", 1810, 1); // A == 1
+    printf("%x%1x\n", 1811, 2); // B == 2;
+    printf("%x%1x\n", 1812, 10);
+    printf("%x%1x\n", 1813, 5);
+    printf("%x%1x\n", 1814, 6);
+    printf("%x%1x\n", 1815, 7);
+    printf("%x%1x\n", 1816, 8);
+    printf("%x%1x\n", 1817, 3);
+    printf("%x%1x\n", 1818, 4);
+    printf("%x%1x\n", 1819, 5);
+    printf("%x%1x\n", 1820, 13);
+    printf("%x%1x\n", 1821, 1); // A == 1
+    printf("%x%1x\n", 1822, 2); // B == 2;
+    printf("%x%1x\n", 1823, 3);
+    printf("%x%1x\n", 1824, 11);
+    printf("%x%1x\n", 1825, 6);
+    printf("%x%1x\n", 1826, 7);
+    printf("%x%1x\n", 1827, 1); // A == 1
+    printf("%x%1x\n", 1828, 9);
+    printf("%x%1x\n", 1829, 4);
+    printf("%x%1x\n", 1830, 5);
+    printf("%x%1x\n", 1831, 6);
+    printf("%x%1x\n", 1832, 14);
+    printf("%x%1x\n", 1833, 2); // B == 2;
+    printf("%x%1x\n", 1834, 3);
+    printf("%x%1x\n", 1835, 4);
+    printf("%x%1x\n", 1836, 12);
+    printf("%x%1x\n", 1837, 7);
+    printf("%x%1x\n", 1838, 1); // A == 1
+    printf("%x%1x\n", 1839, 2); // B == 2;
+    printf("%x%1x\n", 1840, 10);
+    printf("%x%1x\n", 1841, 5);
+    printf("%x%1x\n", 1842, 6);
+    printf("%x%1x\n", 1843, 7);
+    printf("%x%1x\n", 1844, 8);
+    printf("%x%1x\n", 1845, 3);
+    printf("%x%1x\n", 1846, 4);
+    printf("%x%1x\n", 1847, 5);
+    printf("%x%1x\n", 1848, 13);
+    printf("%x%1x\n", 1849, 1); // A == 1
+    printf("%x%1x\n", 1850, 2); // B == 2;
+    printf("%x%1x\n", 1851, 3);
+    printf("%x%1x\n", 1852, 11);
+    printf("%x%1x\n", 1853, 6);
+    printf("%x%1x\n", 1854, 7);
+    printf("%x%1x\n", 1855, 1); // A == 1
+    printf("%x%1x\n", 1856, 9);
+    printf("%x%1x\n", 1857, 4);
+    printf("%x%1x\n", 1858, 5);
+    printf("%x%1x\n", 1859, 6);
+    printf("%x%1x\n", 1860, 14);
+    printf("%x%1x\n", 1861, 2); // B == 2;
+    printf("%x%1x\n", 1862, 3);
+    printf("%x%1x\n", 1863, 4);
+    printf("%x%1x\n", 1864, 12);
+    printf("%x%1x\n", 1865, 7);
+    printf("%x%1x\n", 1866, 1); // A == 1
+    printf("%x%1x\n", 1867, 2); // B == 2;
+    printf("%x%1x\n", 1868, 10);
+    printf("%x%1x\n", 1869, 5);
+    printf("%x%1x\n", 1870, 6);
+    printf("%x%1x\n", 1871, 7);
+    printf("%x%1x\n", 1872, 8);
+    printf("%x%1x\n", 1873, 3);
+    printf("%x%1x\n", 1874, 4);
+    printf("%x%1x\n", 1875, 5);
+    printf("%x%1x\n", 1876, 13);
+    printf("%x%1x\n", 1877, 1); // A == 1
+    printf("%x%1x\n", 1878, 2); // B == 2;
+    printf("%x%1x\n", 1879, 3);
+    printf("%x%1x\n", 1880, 11);
+    printf("%x%1x\n", 1881, 6);
+    printf("%x%1x\n", 1882, 7);
+    printf("%x%1x\n", 1883, 1); // A == 1
+    printf("%x%1x\n", 1884, 9);
+    printf("%x%1x\n", 1885, 4);
+    printf("%x%1x\n", 1886, 5);
+    printf("%x%1x\n", 1887, 6);
+    printf("%x%1x\n", 1888, 14);
+    printf("%x%1x\n", 1889, 2); // B == 2;
+    printf("%x%1x\n", 1890, 3);
+    printf("%x%1x\n", 1891, 4);
+    printf("%x%1x\n", 1892, 12);
+    printf("%x%1x\n", 1893, 7);
+    printf("%x%1x\n", 1894, 1); // A == 1
+    printf("%x%1x\n", 1895, 2); // B == 2;
+    printf("%x%1x\n", 1896, 10);
+    printf("%x%1x\n", 1897, 5);
+    printf("%x%1x\n", 1898, 6);
+    printf("%x%1x\n", 1899, 7);
+    printf("%x%1x\n", 1900, 1); // A == 1
+    printf("%x%1x\n", 1901, 2); // B == 2;
+    printf("%x%1x\n", 1902, 3);
+    printf("%x%1x\n", 1903, 4);
+    printf("%x%1x\n", 1904, 12);
+    printf("%x%1x\n", 1905, 7);
+    printf("%x%1x\n", 1906, 1); // A == 1
+    printf("%x%1x\n", 1907, 2); // B == 2;
+    printf("%x%1x\n", 1908, 10);
+    printf("%x%1x\n", 1909, 5);
+    printf("%x%1x\n", 1910, 6);
+    printf("%x%1x\n", 1911, 7);
+    printf("%x%1x\n", 1912, 8);
+    printf("%x%1x\n", 1913, 3);
+    printf("%x%1x\n", 1914, 4);
+    printf("%x%1x\n", 1915, 5);
+    printf("%x%1x\n", 1916, 13);
+    printf("%x%1x\n", 1917, 1); // A == 1
+    printf("%x%1x\n", 1918, 2); // B == 2;
+    printf("%x%1x\n", 1919, 3);
+    printf("%x%1x\n", 1920, 11);
+    printf("%x%1x\n", 1921, 6);
+    printf("%x%1x\n", 1922, 7);
+    printf("%x%1x\n", 1923, 1); // A == 1
+    printf("%x%1x\n", 1924, 9);
+    printf("%x%1x\n", 1925, 4);
+    printf("%x%1x\n", 1926, 5);
+    printf("%x%1x\n", 1927, 6);
+    printf("%x%1x\n", 1928, 14);
+    printf("%x%1x\n", 1929, 2); // B == 2;
+    printf("%x%1x\n", 1930, 3);
+    printf("%x%1x\n", 1931, 4);
+    printf("%x%1x\n", 1932, 12);
+    printf("%x%1x\n", 1933, 7);
+    printf("%x%1x\n", 1934, 1); // A == 1
+    printf("%x%1x\n", 1935, 2); // B == 2;
+    printf("%x%1x\n", 1936, 10);
+    printf("%x%1x\n", 1937, 5);
+    printf("%x%1x\n", 1938, 6);
+    printf("%x%1x\n", 1939, 7);
+    printf("%x%1x\n", 1940, 8);
+    printf("%x%1x\n", 1941, 3);
+    printf("%x%1x\n", 1942, 4);
+    printf("%x%1x\n", 1943, 5);
+    printf("%x%1x\n", 1944, 13);
+    printf("%x%1x\n", 1945, 1); // A == 1
+    printf("%x%1x\n", 1946, 2); // B == 2;
+    printf("%x%1x\n", 1947, 3);
+    printf("%x%1x\n", 1948, 11);
+    printf("%x%1x\n", 1949, 6);
+    printf("%x%1x\n", 1950, 7);
+    printf("%x%1x\n", 1951, 1); // A == 1
+    printf("%x%1x\n", 1952, 9);
+    printf("%x%1x\n", 1953, 4);
+    printf("%x%1x\n", 1954, 5);
+    printf("%x%1x\n", 1955, 6);
+    printf("%x%1x\n", 1956, 14);
+    printf("%x%1x\n", 1957, 2); // B == 2;
+    printf("%x%1x\n", 1958, 3);
+    printf("%x%1x\n", 1959, 4);
+    printf("%x%1x\n", 1960, 12);
+    printf("%x%1x\n", 1961, 7);
+    printf("%x%1x\n", 1962, 1); // A == 1
+    printf("%x%1x\n", 1963, 2); // B == 2;
+    printf("%x%1x\n", 1964, 10);
+    printf("%x%1x\n", 1965, 5);
+    printf("%x%1x\n", 1966, 6);
+    printf("%x%1x\n", 1967, 7);
+    printf("%x%1x\n", 1968, 8);
+    printf("%x%1x\n", 1969, 3);
+    printf("%x%1x\n", 1970, 4);
+    printf("%x%1x\n", 1971, 5);
+    printf("%x%1x\n", 1972, 13);
+    printf("%x%1x\n", 1973, 1); // A == 1
+    printf("%x%1x\n", 1974, 2); // B == 2;
+    printf("%x%1x\n", 1975, 3);
+    printf("%x%1x\n", 1976, 11);
+    printf("%x%1x\n", 1977, 6);
+    printf("%x%1x\n", 1978, 7);
+    printf("%x%1x\n", 1979, 1); // A == 1
+    printf("%x%1x\n", 1980, 9);
+    printf("%x%1x\n", 1981, 4);
+    printf("%x%1x\n", 1982, 5);
+    printf("%x%1x\n", 1983, 6);
+    printf("%x%1x\n", 1984, 14);
+    printf("%x%1x\n", 1985, 2); // B == 2;
+    printf("%x%1x\n", 1986, 3);
+    printf("%x%1x\n", 1987, 4);
+    printf("%x%1x\n", 1988, 12);
+    printf("%x%1x\n", 1989, 7);
+    printf("%x%1x\n", 1990, 1); // A == 1
+    printf("%x%1x\n", 1991, 2); // B == 2;
+    printf("%x%1x\n", 1992, 10);
+    printf("%x%1x\n", 1993, 5);
+    printf("%x%1x\n", 1994, 6);
+    printf("%x%1x\n", 1995, 7);
+    printf("%x%1x\n", 1996, 8);
+    printf("%x%1x\n", 1997, 3);
+    printf("%x%1x\n", 1998, 4);
+    printf("%x%1x\n", 1999, 5);
+    printf("%x%1x\n", 2000, 13);
+    printf("%x%1x\n", 2001, 1); // A == 1
+    printf("%x%1x\n", 2002, 2); // B == 2;
+    printf("%x%1x\n", 2003, 3);
+    printf("%x%1x\n", 2004, 11);
+    printf("%x%1x\n", 2005, 6);
+    printf("%x%1x\n", 2006, 7);
+    printf("%x%1x\n", 2007, 1); // A == 1
+    printf("%x%1x\n", 2008, 9);
+    printf("%x%1x\n", 2009, 4);
+    printf("%x%1x\n", 2010, 5);
+    printf("%x%1x\n", 2011, 6);
+    printf("%x%1x\n", 2012, 14);
+    printf("%x%1x\n", 2013, 2); // B == 2;
+    printf("%x%1x\n", 2014, 3);
+    printf("%x%1x\n", 2015, 4);
+    printf("%x%1x\n", 2016, 12);
+    printf("%x%1x\n", 2017, 7);
+    printf("%x%1x\n", 2018, 1); // A == 1
+    printf("%x%1x\n", 2019, 2); // B == 2;
+    printf("%x%1x\n", 2020, 10);
+    printf("%x%1x\n", 2021, 5);
+    printf("%x%1x\n", 2022, 6);
+    printf("%x%1x\n", 2023, 7);
+    printf("%x%1x\n", 2024, 8);
+    printf("%x%1x\n", 2025, 3);
+    printf("%x%1x\n", 2026, 4);
+    printf("%x%1x\n", 2027, 5);
+    printf("%x%1x\n", 2028, 13);
+    printf("%x%1x\n", 2029, 1); // A == 1
+    printf("%x%1x\n", 2030, 2); // B == 2;
+    printf("%x%1x\n", 2031, 3);
+    printf("%x%1x\n", 2032, 11);
+    printf("%x%1x\n", 2033, 6);
+
+    return 0;
+}
