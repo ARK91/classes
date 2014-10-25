39dc06f175b20bfb3712b10bb4cc4e92e51d029b
John Hubbard <jhubbard@nvidia.com>
2014-10-24 23:55:48 -0700

    Save point: bin2bcd

--- ucsc/verilog_0764/hw5/f_bin2bcd/bin2bcd.v
+++ ucsc/verilog_0764/hw5/f_bin2bcd/bin2bcd.v
@@ -5,9 +5,36 @@
 
 // Structural Verilog version, as required by the problem statement:
 
-module bin2bcd(bin, bcd);
+module shift_add3(in, result);
+    input [3:0] in;
+    output [3:0] result;
+    reg [3:0] result;
+
+    always@(*)
+        case (in)
+            4'b0000: result = 4'b0000;
+            4'b0001: result = 4'b0001;
+            4'b0011: result = 4'b0011;
+            4'b0100: result = 4'b0100;
+
+            4'b0000: result = 4'b0000;
+            4'b0101: result = 4'b1000;
+            4'b0110: result = 4'b1001;
+            4'b0000: result = 4'b0000;
+
+            4'b0111: result = 4'b1010;
+            4'b1000: result = 4'b1011;
+            4'b1001: result = 4'b0000;
+            default: result = 4'b0000;
+        endcase
+endmodule
+
+
+module bin2bcd(bin, bcdHundreds, bcdTens, bcdOnes);
     input [3:0]bin;
-    output [3:0]bcd;
+    output [3:0]bcdHundreds;
+    output [3:0]bcdTens;
+    output [3:0]bcdOnes;
 
     wire x0, x1, x2, x3, x4, x5;
     wire [3:0]not_bin;
@@ -20,19 +47,19 @@ module bin2bcd(bin, bcd);
     // bit 0:
     and a0(x0,   bin[0], not_bin[1]);
     and a1(x1,   not_bin[0], bin[1]);
-    or g0(bcd[0], x0, x1);
+    or g0(bcdOnes[0], x0, x1);
 
     // bit 1:
     and a2(x2,   bin[1], not_bin[2]);
     and a3(x3,   not_bin[1], bin[2]);
-    or g1(bcd[1], x2, x3);
+    or g1(bcdOnes[1], x2, x3);
 
     // bit 2:
     and a4(x4,   bin[2], not_bin[3]);
     and a5(x5,   not_bin[2], bin[3]);
-    or g2(bcd[2], x4, x5);
+    or g2(bcdOnes[2], x4, x5);
 
     // bit 3:
-    buf g3(bcd[3], bin[3]);
+    buf g3(bcdOnes[3], bin[3]);
 endmodule
 
--- ucsc/verilog_0764/hw5/f_bin2bcd/bin2bcd_tb.v
+++ ucsc/verilog_0764/hw5/f_bin2bcd/bin2bcd_tb.v
@@ -13,17 +13,19 @@
 `timescale 1ns/1ns
 module bin2bcd_tb;
     reg [3:0]bin_tb;
-    wire [3:0]bcd_tb;
+    wire [3:0]bcdHundreds_tb;
+    wire [3:0]bcdTens_tb;
+    wire [3:0]bcdOnes_tb;
     integer count;
 
-    bin2bcd DUT(bin_tb, bcd_tb);
+    bin2bcd DUT(bin_tb, bcdHundreds_tb, bcdTens_tb, bcdOnes_tb);
 
     // This block prints whenever the inputs or outputs change:
     initial
     begin
-        $monitor("Time %t: bin: %b%b%b%b bcd: %b%b%b%b ",
+        $monitor("Time %t: bin: %b%b%b%b bcd: %h %h %h ",
                  $time, bin_tb[3], bin_tb[2], bin_tb[1], bin_tb[0],
-                 bcd_tb[3], bcd_tb[2], bcd_tb[1], bcd_tb[0]);
+                 bcdHundreds_tb, bcdTens_tb, bcdOnes_tb);
     end
 
     // Test pattern: exhaustive inputs:
