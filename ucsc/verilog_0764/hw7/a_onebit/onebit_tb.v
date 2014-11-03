a5626d7258e10f0777f9bb02dcc6af5136c8677a
John Hubbard <jhubbard@nvidia.com>
2014-11-02 22:01:50 -0800

    Polished test bench, added schematic for N == 4

--- ucsc/verilog_0764/hw7/a_onebit/onebit_tb.v
+++ ucsc/verilog_0764/hw7/a_onebit/onebit_tb.v
@@ -41,9 +41,7 @@ module onebit_tb;
     // 8-bit test pattern:
     initial
     begin
-        // Expect failure:
-        number_tb     = 8'b00000000;
-        // Expect success:
+        #10 $display("Succcess cases:");
         #10 number_tb = 8'b00000001;
         #10 number_tb = 8'b00000010;
         #10 number_tb = 8'b00000100;
@@ -53,15 +51,55 @@ module onebit_tb;
         #10 number_tb = 8'b01000000;
         #10 number_tb = 8'b10000000;
 
-        // Expect failure:
+        #10 $display("Failure cases:");
+        #10 number_tb = 8'b00000000;
         #10 number_tb = 8'b00001001;
         #10 number_tb = 8'b00011001;
         #10 number_tb = 8'b10001001;
         #10 number_tb = 8'b10000001;
+        #10 number_tb = 8'b11111111;
+        #10 number_tb = 8'b11101111;
+        #10 number_tb = 8'b01111111;
+        #10 number_tb = 8'b11111110;
+        #10 number_tb = 8'b01111110;
     end
 
 endmodule
 
 /* Sample test run:
 
+vlog ../*.v
+# Model Technology ModelSim ALTERA vlog 10.1e Compiler 2013.06 Jun 12 2013
+# -- Compiling module tiny_onebit
+# -- Compiling module onebit
+# -- Compiling module onebit_tb
+#
+# Top level modules:
+#   onebit_tb
+vsim work.onebit_tb
+# vsim work.onebit_tb
+# Loading work.onebit_tb
+# Loading work.onebit
+# Loading work.tiny_onebit
+run 300ns
+# Succcess cases:
+# number: 00000001, result: 1
+# number: 00000010, result: 1
+# number: 00000100, result: 1
+# number: 00001000, result: 1
+# number: 00010000, result: 1
+# number: 00100000, result: 1
+# number: 01000000, result: 1
+# number: 10000000, result: 1
+# Failure cases:
+# number: 00000000, result: 0
+# number: 00001001, result: 0
+# number: 00011001, result: 0
+# number: 10001001, result: 0
+# number: 10000001, result: 0
+# number: 11111111, result: 0
+# number: 11101111, result: 0
+# number: 01111111, result: 0
+# number: 11111110, result: 0
+# number: 01111110, result: 0
 */
