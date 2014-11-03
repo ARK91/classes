36ae92fe6027c3a856f3f2303d17887c013e215e
John Hubbard <jhubbard@nvidia.com>
2014-11-02 19:45:35 -0800

    Code complete

--- ucsc/verilog_0764/hw7/a_onebit/onebit.v
+++ ucsc/verilog_0764/hw7/a_onebit/onebit.v
@@ -4,18 +4,35 @@
 // one bit is set.
 //
 
-`timescale 1ns/1ns
+module tiny_onebit(a, b, f, all_ones);
+    input a, b;
+    output f, all_ones;
+
+    assign f = a ^ b;
+    assign all_ones = a & b;
+endmodule
+
 module onebit(number, result);
     parameter N = 2;
     input [N-1:0] number;
-    output  result;
+    output result;
 
-    assign result = 1;
+    wire no_all_one_failures;
+    wire [N-1:0] all_ones;
+    wire [N-1:0] f;
 
-    always @(*)
-    begin
-        //x
-    end
+    nor xNoAllOnesFailures(no_all_one_failures, all_ones);
+    and xResult(result, no_all_one_failures, f[N-1]);
+
+    tiny_onebit ONEBIT(number[0], number[1], all_ones[0], f[0]);
+
+    genvar i;
+    generate
+        for (i = 1; i < N; i = i + 1)
+        begin: genOneBit
+            tiny_onebit genOneBit(f[i-1], number[i+1], all_ones[i], f[i]);
+        end
+    endgenerate
 
 endmodule
 
