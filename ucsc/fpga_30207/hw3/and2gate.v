6f0cd1a02368512b52c8f8af719a6b575695b0b0
John Hubbard <jhubbard@nvidia.com>
2015-01-31 16:39:41 -0800

    Fixed a typo bug in and2gate

--- ucsc/fpga_30207/hw3/and2gate.v
+++ ucsc/fpga_30207/hw3/and2gate.v
@@ -5,7 +5,7 @@ module and2gate(a,b,f) ;
     assign f = a & b ;
 endmodule
 
-module a7_and2gate(sw,Led) ;
+module a7_and2gate(sw, led) ;
     input [7:6] sw;
     output [4:4] led ;
     and2gate U1(sw[7],sw[6],led[4]) ;
