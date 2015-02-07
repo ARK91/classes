516be2f2a28df53b8290307560fee6a9a0bf593b
John Hubbard <jhubbard@nvidia.com>
2015-02-06 21:37:31 -0800

    Fixed to use wire and integer data types, avoiding parameter overflow.

--- ucsc/fpga_30207/hw3/counter.v
+++ ucsc/fpga_30207/hw3/counter.v
@@ -1,13 +1,25 @@
- //File onesecond_bad.v
- //hex2_7seg.v onesecond_bad.v basys3.xdc
-// Modified by John Hubbard, 31 Jan 2015
-// Added an intermediate clock, so that there is one for seconds, and that
-// clock cascades into the per-minute clock. This avoids overflowing the
-// parameter (which appears to be a 32-bit value).
+// File counter.v
+// Also uses: hex2_7seg.v basys3.xdc
+// Modified (and fixed) by John Hubbard, 06 Feb 2015
+//
+// Used integer and 34-bit wire variables, to hold the large counter values.
+// When counting up to 60 seconds, using a 100 MHz clock, you need to count
+// up to 60*100*1,000,000 = 6 billion, which requires at least 33 bits.
+//
+// The original code used "parameter" to hold this, but the parameter keyword
+// is only necessary when a value needs to be changed at instantiation time.
+// It appears that only 32 bits wide parameters are supported, which led to a
+// bug that was invisible for a one-second (100 * 1,000,000 : 27 bits) counter,
+// but overflowed a 32-bit counter.
+//
+// The fix, again, is simply to use wire and integer types of large enough bit
+// width.
+//
+// --John Hubbard, 06 Feb 2015
 //
 // For HW #3 assignment of FPGA class.
 
-module counter(clk,arst,q) ;
+module counter(clk, arst, q) ;
      parameter N = 7 ;
      input clk,arst ;
      output [N-1:0] q ;
@@ -20,32 +32,27 @@ module counter(clk,arst,q) ;
             q <= q + 1 ;
 endmodule
 
-module onesecond_bad(clk, btnU, seg, an, led) ;
-    parameter C = 28 ; //28-1 = 27. Note counter is also N-1
+module oneminute_bad(clk, btnU, seg, an, led) ;
+    parameter C = 34 ; // 36 bits is ample to count up 60 * 100 MHz.
     parameter N7 = 7 ;
     parameter N4 = 4 ;
-    parameter CRYSTAL = 100 ; // 100 MHZ
 
-    parameter STOPAT_SEC_PER_MIN = 60;
-    parameter STOPAT_ONE_SEC = (CRYSTAL * 1_000_000 * 1)- 1;
+    integer CRYSTAL = 100 ; // 100 MHZ
+    wire  [C-1:0]STOPAT_ONE_MIN = (CRYSTAL * 1_000_000 * 60)- 1;
 
     input clk, btnU;
     output [0:N7-1] seg ;
     output [N4-1:0] an ;
     output [N4-1:0] led ;
-    wire [C-1:0] min_counter ;
     wire [C-1:0] sec_counter ;
     wire [N4-1:0] zero_to_f_counter ;
-    wire one_second_clock ;
     wire one_minute_clock ;
 
     assign an = 4'b0010 ; /* ON ON OFF ON */
-    assign one_second_clock = (sec_counter == STOPAT_ONE_SEC) ? 1: 0 ;
-    assign one_minute_clock = (min_counter == STOPAT_SEC_PER_MIN) ? 1: 0 ;
+    assign one_minute_clock = (sec_counter == STOPAT_ONE_MIN) ? 1: 0 ;
 
-    counter #C FINAL_CLOCK(one_second_clock,(btnU|| one_minute_clock),min_counter) ;
-    counter #C MEDIUM_CLOCK(clk,(btnU|| one_second_clock),sec_counter) ;
-    counter #N4 S(one_minute_clock,btnU,zero_to_f_counter) ;
+    counter #C MINUTE_COUNTER(clk, (btnU || one_minute_clock), sec_counter) ;
+    counter #N4 HEX_COUNTER(one_minute_clock, btnU, zero_to_f_counter) ;
 
     hex2_7seg D(zero_to_f_counter,seg);
 
