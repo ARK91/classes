cf81353bc44cc8141af139f00ba8584a23a50075
John Hubbard <jhubbard@nvidia.com>
2015-02-17 00:37:32 -0800

    Minor refactoring of where counters go in the source files

--- ucsc/fpga_30207/hw4/onsecond_good.v
+++ ucsc/fpga_30207/hw4/onsecond_good.v
@@ -1,49 +1,6 @@
 // File: onesecond_good.v
 // Modified by John Hubbard, 31 Jan 2015
 
-module counter(clk, arst, en, q) ;
-    parameter N = 7 ;
-    input clk,arst,en ;
-    output [N-1:0] q ;
-    reg [N-1:0] q ;
-
-    always @(posedge clk or posedge arst)
-    if (arst == 1'b1)
-        q <= 0 ;
-    else if (en)
-        q <= q + 1 ;
-endmodule
-
-module mod_counter(clk, arst, q, done) ;
-    parameter N = 7 ;
-    parameter MAX = 127 ;
-    input clk,arst;
-    output [N-1:0] q ;
-    output done ;
-
-    reg [N-1:0] q ;
-    reg done ;
-
-    always @(posedge clk or posedge arst)
-    begin
-        if (arst == 1'b1)
-        begin
-            q <= 0 ;
-            done <= 0 ;
-        end
-        else if (q == MAX)
-        begin
-            q <= 0 ;
-            done <= 1 ;
-        end
-        else
-        begin
-            q <= q + 1 ;
-            done <= 0 ;
-        end
-    end
-endmodule
-
 module onesecond_good(clk, btnU, seg, an, led) ;
     parameter C = 28; //27..0 counter
     parameter N7 = 7 ;
