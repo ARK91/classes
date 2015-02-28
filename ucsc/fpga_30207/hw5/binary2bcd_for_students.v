635408da9009f891f751010412a19115e95d9e51
John Hubbard <jhubbard@nvidia.com>
2015-02-27 21:55:30 -0800

    hw5: test bench for modelsim is working

--- ucsc/fpga_30207/hw5/binary2bcd_for_students.v
+++ ucsc/fpga_30207/hw5/binary2bcd_for_students.v
@@ -1,88 +1,85 @@
 `timescale 1ns/1ns
 
 module binary2bcdBehavioral(number,text) ;
-parameter N = 16 ;
-parameter W = 4 ;
-input [N-1:0] number ;
-output [N-1:0] text ;
+    parameter N = 16 ;
+    parameter W = 4 ;
+    input [N-1:0] number ;
+    output [N-1:0] text ;
 
-// WRITE CODE HERE
+    // WRITE CODE HERE
 endmodule
 
 module binary2bcdStructral(number,text) ;
-parameter N = 16 ;
-parameter W = 4 ;
-input [N-1:0] number ;
-output [N-1:0] text ;
+    parameter N = 16 ;
+    parameter W = 4 ;
+    input [N-1:0] number ;
+    output [N-1:0] text ;
 
-// WRITE CODE HERE
+    // WRITE CODE HERE
 endmodule
 
 module D(n,q,r);
-parameter K = 32 ;
-input [K-1:0] n ;
-output [K-1:0] q ;
-output [3:0] r ;
+    parameter K = 32 ;
+    input [K-1:0] n ;
+    output [K-1:0] q ;
+    output [3:0] r ;
 
-// WRITE CODE HERE
+    // WRITE CODE HERE
 endmodule
 
 
 module binary2bcdDivision(number,text) ;
-parameter N = 16 ;
-parameter W = 4 ;
-input [N-1:0] number ;
-output [N-1:0] text ;
+    parameter N = 16 ;
+    parameter W = 4 ;
+    input [N-1:0] number ;
+    output [N-1:0] text ;
 
-//WRITE CODE HERE
+    reg [N-1 :0] workingNumber ;
+    reg [N-1:0] text ;
+
+    reg [3:0] digit;
+    integer digitEnd;
+
+    always @(number)
+    begin
+        text          = 16'h0000;
+        workingNumber = number;
+        digit         = workingNumber % 10;
+        digitEnd      = 3;
+
+        while(workingNumber > 0 && (digitEnd < N))
+        begin
+            text[digitEnd-:4] = digit;
+
+            // Move to the next digit:
+            workingNumber = workingNumber / 10;
+            digit = workingNumber % 10;
+            digitEnd = digitEnd + 4;
+        end
+    end
 endmodule
 
+module binary2bcd_tb();
+    parameter N = 16 ;
+    reg [N-1:0] vec ;
+    wire [N-1:0] text ;
+    wire [N-1:0] text1 ;
+    wire [N-1:0] text2 ;
+    wire [N-1:0] text3 ;
 
+    binary2bcdDivision BCD(vec, text);
+    binary2bcdDivision BCD2(vec, text1);
 
-
-//module binary2bcd(number, text);
-//    parameter N = 16 ;
-//    parameter W = 4 ;
-//    input [N-1:0] number ;
-//    reg [N-1 :0] workingNumber ;
-//    output reg [N-1:0] text ;
-//
-//    reg [3:0] digit;
-//    integer digitEnd;
-//
-//    always @(number)
-//    begin
-//        text          = 16'h0000;
-//        workingNumber = number;
-//        digit         = workingNumber % 10;
-//        digitEnd      = 3;
-//
-//        while(workingNumber > 0 && (digitEnd < N))
-//        begin
-//            text[digitEnd-:4] = digit;
-//
-//            // Move to the next digit:
-//            workingNumber = workingNumber / 10;
-//            digit = workingNumber % 10;
-//            digitEnd = digitEnd + 4;
-//        end
-//    end
-//endmodule
-//
-//module binary2bcd_tb();
-//    parameter N = 16 ;
-//    reg [N-1:0] vec ;
-//    wire [N-1:0] text ;
-//
-//    binary2bcd BCD(vec, text);
-//
-//    initial
-//    begin
-//        for (vec = 0; vec < 102; vec = vec + 1)
-//        begin
-//            #1 $display("vec: %d, text: %h", vec, text);
-//        end
-//    end
-//
-//endmodule
+    initial
+    begin
+        for (vec = 0; vec < 102; vec = vec + 1)
+        begin
+            #1;
+            if (text == text1)
+                #1 $display("vec: %d, text: %h", vec, text);
+            else
+                $display("FAILURE: text: %d, text1: %d", text, text1);
+        end
+    end
+endmodule
 
