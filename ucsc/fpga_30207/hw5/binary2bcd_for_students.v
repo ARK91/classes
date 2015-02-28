a0806828ad645fe9270cda473e3c067fa6a83c40
John Hubbard <jhubbard@nvidia.com>
2015-02-28 01:48:11 -0800

    HW5 done

--- ucsc/fpga_30207/hw5/binary2bcd_for_students.v
+++ ucsc/fpga_30207/hw5/binary2bcd_for_students.v
@@ -32,13 +32,29 @@ module binary2bcdBehavioral(number,text) ;
 endmodule
 
 module add3(a, sum);
-    input [3:0]a;
-    output [3:0]sum;
+    input [0:3]a;
+    output [0:3]sum;
 
-    LUT4 #(16'h03E0) (sum[3], a[0], a[1], a[2], a[3]);
-    LUT4 #(16'h0210) (sum[2], a[0], a[1], a[2], a[3]);
-    LUT4 #(16'h018C) (sum[1], a[0], a[1], a[2], a[3]);
-    LUT4 #(16'h014A) (sum[0], a[0], a[1], a[2], a[3]);
+    reg [3:0] sum;
+    always @(a)
+        case (a)
+            4'b0000: sum <= 4'b0000;
+            4'b1000: sum <= 4'b1000;
+            4'b0100: sum <= 4'b0100;
+            4'b1100: sum <= 4'b1100;
+            4'b0010: sum <= 4'b0010;
+            4'b1010: sum <= 4'b0001;
+            4'b0110: sum <= 4'b1001;
+            4'b1110: sum <= 4'b0101;
+            4'b0001: sum <= 4'b1101;
+            4'b1001: sum <= 4'b0011;
+            default: sum <= 4'b0000;
+        endcase
+
+//  LUT4 #(16'h03E0) S3(sum[3], a[0], a[1], a[2], a[3]);
+//  LUT4 #(16'h0210) S2(sum[2], a[0], a[1], a[2], a[3]);
+//  LUT4 #(16'h018C) S1(sum[1], a[0], a[1], a[2], a[3]);
+//  LUT4 #(16'h014A) S0(sum[0], a[0], a[1], a[2], a[3]);
 endmodule
 
 module add3_tb();
@@ -66,16 +82,16 @@ module binary2bcdStructral(n,text) ;
     wire [0:3] c1_out, c2_out, c3_out, c4_out, c5_out;
 
     buf BUF0(text[0], n[0]);
-    buf BUF1(text[10], 0);
-    buf BUF2(text[11], 0);
+    buf BUF1(text[10], 1'b0);
+    buf BUF2(text[11], 1'b0);
 
-    add3 C1(n[5], n[6], n[7], 0, c1_out);
-    add3 C2(n[4], c1_out[0:2], c2_out);
-    add3 C3(n[3], c2_out[0:2], c3_out);
-    add3 C4(c3_out[3], c2_out[3], c1_out[3], 0, c4_out[0:2], text[9]);
-    add3 C5(n[2], c3_out[0:2], c5_out);
-    add3 C6(c5_out[0], c4_out[0:2], text[5], text[6], text[7], text[8]);
-    add3 C7(n[1], c5_out[0:2], text[1], text[2], text[3], text[4]);
+    add3 C1({n[5],      n[6], n[7], 1'b0}, c1_out);
+    add3 C2({n[4],      c1_out[0:2]}, c2_out);
+    add3 C3({n[3],      c2_out[0:2]}, c3_out);
+    add3 C4({c3_out[3], c2_out[3], c1_out[3], 1'b0}, {c4_out[0:2], text[9]});
+    add3 C5({n[2],      c3_out[0:2]}, c5_out);
+    add3 C6({c5_out[3], c4_out[0:2]}, {text[5], text[6], text[7], text[8]});
+    add3 C7({n[1],      c5_out[0:2]}, {text[1], text[2], text[3], text[4]});
 endmodule
 
 module d10(n,q,r);
@@ -117,17 +133,10 @@ module binary2bcd_tb();
 
     initial
     begin
-        for (vec = 0; vec < 102; vec = vec + 1)
+        for (vec = 0; vec < 1000; vec = vec + 1)
         begin
-            #1;
-            if (text == text1)
-                #1 $display("vec: %d, text: %h", vec, text);
-            else
-                $display("FAILURE: text: %d, text1: %d", text, text1);
-
-            if (text != text2)
-                $display("FAILURE: text: %d, text2: %d", text, text1);
-
+            #1 $display("vec: %d, text: %h, text1: %h, text2: %h",
+                        vec, text, text1, text2);
         end
     end
 endmodule
