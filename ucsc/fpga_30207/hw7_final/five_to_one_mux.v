046d621a5d5abaef8da67740b3c71a1464ee0045
John Hubbard <jhubbard@nvidia.com>
2015-03-22 16:12:40 -0700

    5-to-1 mux tested in modelsim and working

--- ucsc/fpga_30207/hw7_final/five_to_one_mux.v
+++ ucsc/fpga_30207/hw7_final/five_to_one_mux.v
@@ -28,22 +28,24 @@ module mux_tb();
     parameter MAX_VEC = 32;
     integer vec;
     reg a, b, c, d, e;
-    reg [4-1:0] s;
+    reg [3-1:0] s;
+    reg [3-1:0] sLoop;
     wire f;
 
     five_to_one_mux_using_case DUT(a, b, c, d, e, s, f);
 
     initial begin
 
-        for (s = 0; s < 9; s = s + 1) begin
+        for (sLoop = 0; sLoop < 9; sLoop = sLoop + 1) begin
+            s = sLoop[3:0];
             #1;
-            $display("----- s: %b -----------", s);
+            $display("----- sLoop: %b -----------", sLoop);
 
             for (vec = 0; vec < MAX_VEC; vec = vec + 1) begin
                 {e,d,c,b,a} = vec;
 
                 #1;
-                $display("{e,d,c,b,a}: %b, s: %b, f: %b", vec, s, f);
+                $display("{e,d,c,b,a}: %b, s: %b, f: %b", vec[5-1:0], s, f);
             end
         end
     end
