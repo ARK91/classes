0c364bc591cd44ac85e36a766ca86a95fb36329e
John Hubbard <jhubbard@nvidia.com>
2015-03-26 13:58:56 -0700

    Code complete for test_mux_on_board module

--- ucsc/fpga_30207/hw7_final/mux_on_board_tb.v
+++ ucsc/fpga_30207/hw7_final/mux_on_board_tb.v
@@ -51,4 +51,59 @@ module mux_tb();
 
 endmodule
 
+module test_mux_on_board(clk, btnU, sw, seg, an, led);
+    parameter NINPUTS = 256;
+    parameter MAXERROR = 9;
+
+    input clk, btnU;
+    input [15:0]sw;
+    output [0:6] seg;
+    output [3:0] an;
+    output[15:0] led;
+
+    wire done;
+    reg [NINPUTS-1:0] inputs, i;
+    wire o1, o2, o3;
+    wire [15:0] packedHex;
+    reg [3:0] error;
+
+    always @(posedge clk or posedge btnU) begin
+        if (btnU) begin
+            inputs = 0;
+            i = 0;
+        end
+        else if (done) begin
+            i = i + 1;
+            if (i % 10 == 0)
+                inputs = ~(0);
+            else
+                inputs = i;
+        end
+    end
+
+    always @(posedge clk)
+    if (btnU)
+        error = 0;
+    else if (done)
+        if (error < MAXERROR)
+            if ( (o1 != o2) || (o1 != o3) || (o2 != o3) )
+                error = error + 1;
+
+    //o1(4), o2(4), o3(4), first(4)
+    //0 or 1 0 or 1 <3 to 0> <2 to 0>
+    assign text = btnU ? 0 : ((inputs % 10) == 0) ? ~(0) :
+                    {{3'b0, o1}, {3'b0,o2},{3'b0,o3}, error};
+    //a,b,c,d,e
+    //0 1 2 3 4
+    five_to_one_mux_using_case DUT1(a, b, c, d, e, s, o1);
+    five_to_one_mux_using_if   DUT2(a, b, c, d, e, s, o2);
+
+    display_packed_hex_for_n_seconds #(0.5) D(clk, btnU, seg, an,
+                                              packedHex, done);
+    assign led = inputs;
+
+    // Provide error injection: if sw0 is on, then tie o3 to 1'b1. That will
+    // generate errors. Otherwise, o2 tracks o2, avoiding any errors:
+    assign o3 = (sw[0] ? 1'b1 : o2);
+endmodule
 
