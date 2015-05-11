95485799bd4bf2a3c7c6832628471bbd918aa42c
John Hubbard <jhubbard@nvidia.com>
2015-05-10 17:13:42 -0700

    SystemVerilog: lab5 randseq: code complete

--- ucsc/system_verilog_50548/lab5-randomize_randsequence/test.sv
+++ ucsc/system_verilog_50548/lab5-randomize_randsequence/test.sv
@@ -1,19 +1,35 @@
 module test();
-   int numPackets;
+    int numPackets;
 
-   task gen_packet();
-	// insert you randsequence code here
-   endtask
+    task gen_packet();
+    // insert you randsequence code here
+        randsequence()
+            TRAFFIC:            PACKET_TYPE PACKET_GAP PACKET_PRIORITY;
+            PACKET_TYPE:        ETHERNET IPV4 IPV6;
+            PACKET_GAP:         LARGE_GAP NORMAL_GAP SMALL_GAP;
+            PACKET_PRIORITY:    PREMIUM ELITE PLATINUM;
 
+            ETHERNET:   { $display("ETHERNET"); };
+            IPV4:       { $display("IPV4"); };
+            IPV6:       { $display("IPV6"); };
 
-   initial begin
-     #10 numPackets = $urandom_range(16, 32);
-         for(int i=0; i< numPackets ; i++) begin
-             $write("Packet ID %0d : ", i);
-             gen_packet(); 
-             $display("\n");
-         end
-          
-   end
+            LARGE_GAP:  { $display("LARGE_GAP"); };
+            NORMAL_GAP: { $display("NORMAL_GAP"); };
+            SMALL_GAP:  { $display("SMALL_GAP"); };
+
+            PREMIUM:    { $display("PREMIUM SERVICE"); };
+            ELITE:      { $display("ELITE SERVICE"); };
+            PLATINUM:   { $display("PLATINUM SERVICE"); };
+        endsequence
+    endtask
+
+    initial begin
+        #10 numPackets = $urandom_range(16, 32);
+        for(int i=0; i< numPackets ; i++) begin
+            $write("Packet ID %0d : ", i);
+            gen_packet();
+            $display("\n");
+        end
+    end
 
 endmodule
