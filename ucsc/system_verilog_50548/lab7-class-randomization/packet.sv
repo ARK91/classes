c3817d6b752ffb6b25b7cfd8f7151389842e295e
John Hubbard <jhubbard@nvidia.com>
2015-05-18 16:52:35 -0700

    SystemVerilog lab7: basic printing works again

--- ucsc/system_verilog_50548/lab7-class-randomization/packet.sv
+++ ucsc/system_verilog_50548/lab7-class-randomization/packet.sv
@@ -2,33 +2,32 @@
 
 module packet_tb();
     class packet;
-        rand bit [7:0] src;
-        rand bit [7:0] dst;
-        rand bit [15:0] pktid;
+        rand  bit [7:0] src;
+        rand  bit [7:0] dst;
+        rand  bit [15:0] pktid;
         randc bit [1:0] pri;
 
         task print();
-            @* begin
-                $display("src: %d, dst: %d, pktid: %d, pri: %d",
-                         src, dst, pktid, pri);
-            end
+            $display("src: %d, dst: %d, pktid: %d, pri: %d",
+                     src, dst, pktid, pri);
         endtask
 
         function void post_randomize();
-            $display("src: %d, dst: %d, pktid: %d, pri: %d",
+            $display("post_randomize: src: %d, dst: %d, pktid: %d, pri: %d",
                      src, dst, pktid, pri);
         endfunction
     endclass
 
     initial begin
-        $display("Hello world 1");
         packet onePacket;
+        randomize();
+
+        $display("Hello world 1");
 
         for (int i = 0; i < 8; ++i) begin
             #1;
              onePacket = new();
              onePacket.print();
-             $display("Hello world");
         end
     end
 
