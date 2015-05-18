feb2a2529de80c871d16d7e8458485557e13e2f1
John Hubbard <jhubbard@nvidia.com>
2015-05-17 23:52:52 -0700

    SystemVerilog lab7: packet work in progress

--- ucsc/system_verilog_50548/lab7-class-randomization/packet.sv
+++ ucsc/system_verilog_50548/lab7-class-randomization/packet.sv
@@ -1,5 +1,35 @@
-module packet();
-  class packet;
-  // <insert you code here>
-  endclass
+`timescale 1ns/1ns
+
+module packet_tb();
+    class packet;
+        rand bit [7:0] src;
+        rand bit [7:0] dst;
+        rand bit [15:0] pktid;
+        randc bit [1:0] pri;
+
+        task print();
+            @* begin
+                $display("src: %d, dst: %d, pktid: %d, pri: %d",
+                         src, dst, pktid, pri);
+            end
+        endtask
+
+        function void post_randomize();
+            $display("src: %d, dst: %d, pktid: %d, pri: %d",
+                     src, dst, pktid, pri);
+        endfunction
+    endclass
+
+    initial begin
+        $display("Hello world 1");
+        packet onePacket;
+
+        for (int i = 0; i < 8; ++i) begin
+            #1;
+             onePacket = new();
+             onePacket.print();
+             $display("Hello world");
+        end
+    end
+
 endmodule
