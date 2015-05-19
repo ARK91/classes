555512a215a8492e3747d834c8c0efcf082ab7ec
John Hubbard <jhubbard@nvidia.com>
2015-05-18 23:22:37 -0700

    SystemVerilog lab7 completed and working

--- ucsc/system_verilog_50548/lab7-class-randomization/packet.sv
+++ ucsc/system_verilog_50548/lab7-class-randomization/packet.sv
@@ -1,3 +1,7 @@
+// packet.sv
+// John Hubbard, 18 May 2015
+// SystemVerilog Essentials, lab7 homework
+
 `timescale 1ns/1ns
 
 module packet_tb();
@@ -12,19 +16,24 @@ module packet_tb();
             IPV6
         }frame_type;
 
+        bit parity;
         rand frame_type frameType;
         rand  bit [5:0] length;
         randc bit [3:0] ipg; // IPG: Inter-packet gap
         rand  bit [7:0] payload[];
 
         constraint length_rules {
+            // Cannot do this, because this.payload.size() is not a random
+            // variable (according to Riviera PRO):
+            //solve length before payload.size();
+
             length >= 16;
             length <= 64;
+            payload.size() == length;
 
+            // These two lines are ignored, due to the previous line:
             payload.size() >= 8;
             payload.size() <= 128;
-
-            payload.size() == length;
         }
 
         constraint ipg_rules {
@@ -43,22 +52,91 @@ module packet_tb();
 
         endfunction
 
-        task print();
-            $display("length: %d, payload.size(): %d, ipg: %d, Frame type: %s, ",
-                     length, payload.size(), ipg, get_frame_type());
+        task print(int packet_count);
+            $display("packet_count: %d, length: %d, parity: %b, ipg: %d, ",
+                     packet_count, length, parity, ipg,
+                     "Frame type: %s, ",
+                     get_frame_type());
         endtask
+
+        function void pre_randomize();
+            $display("PRE-RANDOMIZE()");
+        endfunction
+
+        function void post_randomize();
+            int index;
+
+            $display("POST-RANDOMIZE()");
+
+            for (index = 0; index < length; index++)
+                parity = parity ^ payload[index][0];
+        endfunction
+
+    endclass
+
+    class constrained_packet extends packet;
+        constraint ipg_rules{
+            ipg == 5;
+            (frameType == BROADCAST) || (frameType == IPV6);
+        }
+    endclass
+
+    class color_packet extends packet;
+        typedef enum {
+            GREEN=22,
+            YELLOW=18,
+            RED=7,
+            BLUE=0,
+            BROWN=31,
+            ORANGE=28
+        } color_t;
+
+        randc color_t color;
+
+        constraint color_rules {
+            color == YELLOW;
+        }
+
+        constraint ipg_rules {
+            ipg == 7;
+        }
+
+        constraint other_rules {
+            length == 17;
+        }
+
+        task print(int packet_count);
+            super.print(packet_count);
+            $display("Length: %d, ipg: %d, Color: %s",
+                     length, ipg, color.name());
+        endtask
+
     endclass
     ////////////////////////////////////////////////////////////
 
     initial begin
-        packet ethernet;
+        // Randomization and constraints homework section (lab7)
+        constrained_packet ethernet;
+        color_packet yellow_packet;
+
         int num_packets = $urandom_range(4, 16);
         ethernet = new();
 
-        for (int i = 0; i < num_packets; ++i) begin
+        for (int packet_count = 0; packet_count < num_packets;
+             ++packet_count) begin
             #1;
              assert(ethernet.randomize());
-             ethernet.print();
+             ethernet.print(packet_count);
+        end
+
+        // Inheritance homework section (lab7):
+         yellow_packet = new();
+
+         for (int packet_count = 0; packet_count < num_packets;
+              ++packet_count) begin
+            #1;
+            assert(yellow_packet.randomize());
+            yellow_packet.print(packet_count);
         end
     end
 
