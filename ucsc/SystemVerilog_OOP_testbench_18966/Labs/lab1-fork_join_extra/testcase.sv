4d04787f212a1763e8457e920aed3a9515d36733
John Hubbard <jhubbard@nvidia.com>
2015-06-25 00:30:10 -0700

    Sample run with finish added (lab1)

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab1-fork_join_extra/testcase.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab1-fork_join_extra/testcase.sv
@@ -45,13 +45,12 @@ module testcase();
 endmodule
 
 /* Sample runs, before and after uncommenting the final block above:
-
 <sc-xterm-23> lab1-fork_join_extra $ run-full-vcs testcase.sv
-Job <240737235> is submitted to queue <o_pri_interactive>.
+Job <240742936> is submitted to queue <o_pri_interactive>.
 <<Waiting for dispatch ...>>
-<<Starting on sc-sim-204-059>>
+<<Starting on sc-sim-204-023>>
                          Chronologic VCS (TM)
-         Version E-2011.03-SP1-2 -- Thu Jun 25 00:18:27 2015
+         Version E-2011.03-SP1-2 -- Thu Jun 25 00:24:53 2015
                Copyright (c) 1991-2011 by Synopsys Inc.
                          ALL RIGHTS RESERVED
 
@@ -68,13 +67,13 @@ Starting vcs inline pass...
 	However, due to incremental compilation, only 1 module needs to be compiled.
 recompiling module testcase because:
 	This module or some inlined child module(s) has/have been modified.
-ld -r -m elf_i386 -o pre_vcsobj_0_1.o --whole-archive pre_vcsobj_0_1.a --no-whole-archive
+ld -r -m elf_i386 -o pre_vcsobj_1_1.o --whole-archive pre_vcsobj_1_1.a --no-whole-archive
 if [ -x ../simv ]; then chmod -x ../simv; fi
-g++  -o ../simv -melf_i386 -m32   -Wl,-whole-archive    -Wl,-no-whole-archive  _vcsobj_1_1.o  5NrI_d.o 5NrIB_d.o SIM_l.o    pre_vcsobj_0_1.o  rmapats_mop.o rmapats.o      /home/tools/vcs/2011.03-SP1-2/linux/lib/libvirsim.so /home/tools/vcs/2011.03-SP1-2/linux/lib/librterrorinf.so /home/tools/vcs/2011.03-SP1-2/linux/lib/libsnpsmalloc.so     /home/tools/vcs/2011.03-SP1-2/linux/lib/libvcsnew.so /home/tools/vcs/2011.03-SP1-2/linux/lib/libuclinative.so         /home/tools/vcs/2011.03-SP1-2/linux/lib/vcs_save_restore_new.o /home/tools/vcs/2011.03-SP1-2/linux/lib/ctype-stubs_32.a -ldl -lm  -lc -lpthread -ldl 
+g++  -o ../simv -melf_i386 -m32   -Wl,-whole-archive    -Wl,-no-whole-archive  _vcsobj_1_1.o  5NrI_d.o 5NrIB_d.o SIM_l.o    pre_vcsobj_1_1.o  rmapats_mop.o rmapats.o      /home/tools/vcs/2011.03-SP1-2/linux/lib/libvirsim.so /home/tools/vcs/2011.03-SP1-2/linux/lib/librterrorinf.so /home/tools/vcs/2011.03-SP1-2/linux/lib/libsnpsmalloc.so     /home/tools/vcs/2011.03-SP1-2/linux/lib/libvcsnew.so /home/tools/vcs/2011.03-SP1-2/linux/lib/libuclinative.so         /home/tools/vcs/2011.03-SP1-2/linux/lib/vcs_save_restore_new.o /home/tools/vcs/2011.03-SP1-2/linux/lib/ctype-stubs_32.a -ldl -lm  -lc -lpthread -ldl
 ../simv up to date
 Chronologic VCS simulator copyright 1991-2011
 Contains Synopsys proprietary information.
-Compiler version E-2011.03-SP1-2; Runtime version E-2011.03-SP1-2;  Jun 25 00:18 2015
+Compiler version E-2011.03-SP1-2; Runtime version E-2011.03-SP1-2;  Jun 25 00:24 2015
 
 time= 3: Delay= 3 Count= 2
 time= 5: Delay= 5 Count= 8
@@ -88,13 +87,18 @@ time=19: All in-flight threads completed
 
 time=24: Delay= 5 Count= 3
 $finish called from file "testcase.sv", line 33.
+time=124: Right before finishing...
+
 $finish at simulation time                  124
-           V C S   S i m u l a t i o n   R e p o r t 
+           V C S   S i m u l a t i o n   R e p o r t
 Time: 124
-CPU Time:      0.060 seconds;       Data structure size:   0.0Mb
-Thu Jun 25 00:18:29 2015
-CPU time: .131 seconds to compile + .022 seconds to elab + .178 seconds to link + .141 seconds in simulation
-<sc-xterm-23> lab1-fork_join_extra $ vi testcase.sv 
+CPU Time:      0.050 seconds;       Data structure size:   0.0Mb
+Thu Jun 25 00:24:55 2015
+CPU time: .106 seconds to compile + .014 seconds to elab + .110 seconds to link + .109 seconds in simulation
+
+-----------------------------------------------
+
+
 <sc-xterm-23> lab1-fork_join_extra $ run-full-vcs testcase.sv
 Job <240739364> is submitted to queue <o_pri_interactive>.
 <<Waiting for dispatch ...>>
