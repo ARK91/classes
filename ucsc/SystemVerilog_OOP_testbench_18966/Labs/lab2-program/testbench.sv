406354dc2cd1e092ac6b815fc11ae05be125fc51
John Hubbard <jhubbard@nvidia.com>
2015-07-01 22:28:41 -0700

    Lab2: typed in testbench connections

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/testbench.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/testbench.sv
@@ -1,5 +1,30 @@
 module testbench();
 
+    parameter WIDTH = 32;
+    logic clk, reset, enable, preload, mode, detect;
+    logic   [WIDTH-1:0]  preload_data;
+    logic   [WIDTH-1:0]  result;
 
+    counter #(WIDTH) DUT(.*);
+
+    initial begin
+        $monitor("t=%3t: result=%2d, detect=%b", $time, result, detect);
+
+        reset = 1;
+        enable = 0;
+        preload = 0;
+        mode = 0;
+
+        @(posedge clk)
+            reset = 0;
+
+        @(posedge clk);
+        enable = 1;
+
+        repeat (10) @(posedge clk);
+        enable = 0;
+
+        repeat (10) @(posedge clk) $finish;
+    end
 
 endmodule
