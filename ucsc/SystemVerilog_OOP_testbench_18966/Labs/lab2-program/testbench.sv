1e2a791eace59634db54411b2f9ab2bcfb093f94
John Hubbard <jhubbard@nvidia.com>
2015-07-01 22:48:45 -0700

    Lab2: page 9 done

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/testbench.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/testbench.sv
@@ -1,9 +1,16 @@
+`timescale 1ns/1ns
+
 module testbench();
 
     parameter WIDTH = 32;
     logic clk, reset, enable, preload, mode, detect;
     logic   [WIDTH-1:0]  preload_data;
     logic   [WIDTH-1:0]  result;
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
 
     counter #(WIDTH) DUT(.*);
 
@@ -28,3 +35,4 @@ module testbench();
     end
 
 endmodule
+
