bfda9b68b63b92496c575d2c5985fdca3669d75a
John Hubbard <jhubbard@nvidia.com>
2015-07-01 23:34:07 -0700

    lab2: Hooked up testcase and testbench

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/testbench.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/testbench.sv
@@ -1,7 +1,6 @@
 `timescale 1ns/1ns
 
 module testbench();
-
     parameter WIDTH = 32;
     logic clk, reset, enable, preload, mode, detect;
     logic   [WIDTH-1:0]  preload_data;
@@ -14,25 +13,7 @@ module testbench();
 
     counter #(WIDTH) DUT(.*);
 
-    initial begin
-        $monitor("t=%3t: result=%2d, detect=%b", $time, result, detect);
-
-        reset = 1;
-        enable = 0;
-        preload = 0;
-        mode = 0;
-
-        @(posedge clk)
-            reset = 0;
-
-        @(posedge clk);
-        enable = 1;
-
-        repeat (10) @(posedge clk);
-        enable = 0;
-
-        repeat (10) @(posedge clk) $finish;
-    end
+    testcase TEST_CASE_1(.*);
 
 endmodule
 
