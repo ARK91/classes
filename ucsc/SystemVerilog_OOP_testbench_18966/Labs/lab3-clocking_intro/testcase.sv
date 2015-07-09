db08df7f23a97ce29afa1bfea77af9dad0a607d7
John Hubbard <jhubbard@nvidia.com>
2015-07-08 20:55:08 -0700

    lab3-clocking-intro done

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab3-clocking_intro/testcase.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab3-clocking_intro/testcase.sv
@@ -1,32 +1,39 @@
+`timescale 1ns/1ns
+
 program testcase();
 
-   bit clk;
-   bit system_clk;
+    bit clk;
+    bit system_clk;
 
-   //Free running clock
+    //Free running clock
+    initial forever #5 clk = ~clk;
+    initial forever #10 system_clk = ~system_clk;
 
+    //Specify the default clocking
+    clocking cb @(posedge clk);
+    endclocking
 
-   //Specify the default clocking
-   clocking cb @ (posedge clk);
-   endclocking
+    default clocking system @(posedge system_clk);
 
+    endclocking
 
-   initial begin
-      $display("t=%5d: Current time is %5t ns", $time, $time);
+    initial begin
+        $display("t=%5d: Current time is %5t ns", $time, $time);
 
-      #10; // <--- this is 10ns!
-      $display("t=%5d: Current time is %5t ns", $time, $time);
+        #10; // <--- this is 10ns!
+        $display("t=%5d: Current time is %5t ns", $time, $time);
 
-      //##10; // <--- this is not 100ns!
-      //$display("t=%5d: Current time is %5t ns", $time, $time);
+        ##10; // <--- this is not 100ns!
+        $display("t=%5d: Current time is %5t ns", $time, $time);
 
-      repeat(10) @(cb); // <--- same as ##10 
-      $display("t=%5d: Current time is %5t ns", $time, $time);
+        repeat(10) @(cb); // <--- same as ##10
+        $display("t=%5d: Current time is %5t ns", $time, $time);
 
-      repeat(10) @(posedge clk); // <--- same as ##10 
-      $display("t=%5d: Current time is %5t ns", $time, $time);
+        repeat(10) @(posedge clk); // <--- same as ##10
+        $display("t=%5d: Current time is %5t ns", $time, $time);
 
-      $finish;
-   end
+        $finish;
+    end
 
 endprogram
+
