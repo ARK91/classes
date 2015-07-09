b609fe50daebc2adc1fdaa3f038d5e381ab3e68a
John Hubbard <jhubbard@nvidia.com>
2015-07-08 20:12:09 -0700

    lab2: Basic python test scoring works, for one test at a time

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/regression/regression-test.py
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab2-program/regression/regression-test.py
@@ -12,6 +12,8 @@
 # ------------------------------------------------------------------------------
 
 import os
+import re
+import subprocess
 from optparse import OptionParser
 
 # --------------- Command-line options -----------------------------------------
@@ -31,11 +33,62 @@ def debug_print_args():
 
 home = os.environ["HOME"]
 
+def extract_scores(log):
+    info = { "fail_count":0, "pass_count":0, "no_report_count":0, "score":0.0 }
+    output = ""
+    for line in log.splitlines():
+        if line.strip().startswith("TEST_RESULT: PASS"):
+            info["pass_count"] += 1
+        elif line.strip().startswith("TEST_RESULT: FAIL"):
+            info["fail_count"] += 1
+        else:
+            info["no_report_count"] += 1
+    return info, output
+
+def print_final_scores(info):
+    score = 0.0
+    total = (info["pass_count"] + info["fail_count"] + info["no_report_count"])
+    if total == 0:
+        return result
+    score = (float(info["pass_count"]) / float(total)) * 100.0
+    print("Test result summary:")
+    print("Passes   : " + str(info["pass_count"]))
+    print("Failures : " + str(info["fail_count"]))
+    print("Score:     %.2f percent passed" % score)
+
+def run_all_tests():
+    print("Running ALL testcases\n")
+
+def run_one_test(testname):
+    print("Running testcase: %s" % testname)
+
+    old_path = os.getcwd()
+    os.chdir("../scenarios/%s" % testname)
+    process = subprocess.Popen("./runsim", stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, shell=True)
+
+    stdout, stderr = process.communicate()
+    if not (type(stdout) is str):
+        # Required for earlier versions of Python:
+        if (type(stdout) is bytes):
+            stdout = stdout.decode("utf-8")
+            stderr = stderr.decode("utf-8")
+
+
+    info, stdout = extract_scores(stdout)
+    for line in stdout.splitlines():
+        print (line)
+    for line in stderr.splitlines():
+        print (line)
+
+    print_final_scores(info)
+    os.chdir(old_path)
+
 def run_commands():
     if opt.run_all_tests != None:
-        print("Running ALL testcases\n")
+        run_all_tests()
     elif opt.testcase_name != None:
-        print("Running testcase: %s" % opt.testcase_name)
+        run_one_test(opt.testcase_name)
     else:
         print("Please run this program with -h or --help for usage information\n")
         exit(1)
