fcfd4e06baf9445375f4b762eb65ce6a0a6fd48a
John Hubbard <jhubbard@nvidia.com>
2015-07-19 14:28:00 -0700

    SystemVerilog OOP Testbench lab4 (interface, clocking): testcase is code complete

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab4-interface_program_clocking/rtl/memory_core.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab4-interface_program_clocking/rtl/memory_core.sv
@@ -1,33 +1,33 @@
 module memory_core (//Inputs
-                    input  bit		clk,
-                    input  logic 	reset,
-                    input  logic	we_mem,
-                    input  logic	ce_mem,
-                    input  logic [7:0]	addr_mem,
-                    input  logic [7:0]	datai_mem,
+                    input  bit          clk,
+                    input  logic        reset,
+                    input  logic        we_mem,
+                    input  logic        ce_mem,
+                    input  logic [7:0]  addr_mem,
+                    input  logic [7:0]  datai_mem,
 
                     //output
-                    output logic [7:0] 	datao_mem
+                    output logic [7:0]  datao_mem
                    );
 
 
-   // Memory array
-   logic [7:0] mem [0:255];
+    // Memory array
+    logic [7:0] mem [0:255];
 
-   //=================================================
-   // Write Logic
-   //=================================================
-   always @ (posedge clk)
+    //=================================================
+    // Write Logic
+    //=================================================
+    always @ (posedge clk)
     if (ce_mem && we_mem) begin
-      mem[addr_mem] <= datai_mem;
+        mem[addr_mem] <= datai_mem;
     end
 
-   //=================================================
-   // Read Logic
-   //=================================================
-   always @ (posedge clk)
+    //=================================================
+    // Read Logic
+    //=================================================
+    always @ (posedge clk)
     if (ce_mem && ~we_mem)  begin
-      datao_mem <= mem[addr_mem];
+        datao_mem <= mem[addr_mem];
     end
 
 endmodule
--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab4-interface_program_clocking/testcases/basic_read_write/testcase.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab4-interface_program_clocking/testcases/basic_read_write/testcase.sv
@@ -1,14 +1,61 @@
 // Stand-alone test inside program block
-program testcase(//Inputs to Design, Outputs from Program <-- Signals are to be driven inside Progam Block
 
-                 //Inout to/from Design, inout to/from Programa <-- Signals are to be driven inside Program
-
-                 //Output from Design, Input to Program <-- Signals are to be monitored
-
+program testcase(input bit clk,
+                 output logic           reset,
+                 output logic           we_sys,
+                 output logic           cmd_valid_sys,
+                 output logic [7:0]     addr_sys,
+                 ref    logic [7:0]     data_sys // VCS workaround. Can't use inout
+                 input logic    ready_sys
                 );
 
-   initial begin
-      ...
-   end
+    initial begin
+        reset         = 1;
+        we_sys        = 0;
+        cmd_valid_sys = 0;
+        addr_sys      = 0;
+        data_sys      = 8'bz;
+
+        #100 reset = 0;
+
+        for (int i = 0; i < 4; i++) begin
+            @(posedge clk);
+            addr_sys      = i;
+            data_sys      = $urandom_range(0, 255);
+            cmd_valid_sys = 1;
+            we_sys        = 1;
+
+            @(posedge ready_sys);
+            $display("%5dns: Writing: address=%0d, data_sys (written)=8'h%2h",
+                     $time, i, data_sys);
+
+            @(posedge clk);
+            addr_sys      = 0;
+            data_sys      = 8'bz;
+            cmd_valid_sys = 0;
+            we_sys        = 0;
+        end
+
+        $display("\n");
+
+        repeat (10) @(posedge clk);
+
+        for (int i = 0; i < 4; i++) begin
+            @(posedge clk);
+            addr_sys      = i;
+            cmd_valid_sys = 1;
+            we_sys        = 0;
+
+            @(posedge ready_sys);
+            @(posedge clk);
+            $display("%5dns: Reading: address=%0d, data_sys(read)=8'h%2h",
+                     $time, i, data_sys);
+
+            addr_sys      = 0;
+            cmd_valid_sys = 0;
+        end
+
+        #10 $finish
+    end
 
 endprogram
