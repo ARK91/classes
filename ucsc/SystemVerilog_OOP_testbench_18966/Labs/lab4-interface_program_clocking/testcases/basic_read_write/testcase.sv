5185236bbb15ff16a5f684d3f950bc8b5008c867
John Hubbard <jhubbard@nvidia.com>
2015-07-19 17:47:28 -0700

    Lab4: activating clocking block, code complete

--- ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab4-interface_program_clocking/testcases/basic_read_write/testcase.sv
+++ ucsc/SystemVerilog_OOP_testbench_18966/Labs/lab4-interface_program_clocking/testcases/basic_read_write/testcase.sv
@@ -12,33 +12,33 @@ program testcase(interface tcif);
 
     initial begin
         // DUT variables:
-        tcif.reset         = 1;
-        tcif.we_sys        = 0;
-        tcif.cmd_valid_sys = 0;
-        tcif.addr_sys      = 0;
-        tcif.data_sys      = 8'bz;
+        tcif.memcb.reset         = 1;
+        tcif.memcb.we_sys        = 0;
+        tcif.memcb.cmd_valid_sys = 0;
+        tcif.memcb.addr_sys      = 0;
+        tcif.memcb.data_sys      = 8'bz;
 
 
-        #100 tcif.reset = 0;
+        #100 tcif.memcb.reset = 0;
 
         for (int i = 0; i < 4; i++) begin
             @(posedge tcif.clk);
-            tcif.addr_sys      = i;
-            tcif.data_sys      = $urandom_range(0, 255);
-            tcif.cmd_valid_sys = 1;
-            tcif.we_sys        = 1;
+            tcif.memcb.addr_sys      = i;
+            tcif.memcb.data_sys      = $urandom_range(0, 255);
+            tcif.memcb.cmd_valid_sys = 1;
+            tcif.memcb.we_sys        = 1;
 
-            @(posedge tcif.ready_sys);
-            $display("%5dns: Writing: address=%0d, tcif.data_sys (written)=8'h%2h",
-                     $time, i, tcif.data_sys);
+            @(posedge tcif.memcb.ready_sys);
+            $display("%5dns: Writing: address=%0d, data_sys (written)=8'h%2h",
+                     $time, i, tcif.memcb.data_sys);
 
             ++nWrites;
 
             @(posedge tcif.clk);
-            tcif.addr_sys      = 0;
-            tcif.data_sys      = 8'bz;
-            tcif.cmd_valid_sys = 0;
-            tcif.we_sys        = 0;
+            tcif.memcb.addr_sys      = 0;
+            tcif.memcb.data_sys      = 8'bz;
+            tcif.memcb.cmd_valid_sys = 0;
+            tcif.memcb.we_sys        = 0;
         end
 
         $display("\n");
@@ -47,19 +47,19 @@ program testcase(interface tcif);
 
         for (int i = 0; i < 4; i++) begin
             @(posedge tcif.clk);
-            tcif.addr_sys      = i;
-            tcif.cmd_valid_sys = 1;
-            tcif.we_sys        = 0;
+            tcif.memcb.addr_sys      = i;
+            tcif.memcb.cmd_valid_sys = 1;
+            tcif.memcb.we_sys        = 0;
 
-            @(posedge tcif.ready_sys);
+            @(posedge tcif.memcb.ready_sys);
             @(posedge tcif.clk);
-            $display("%5dns: Reading: address=%0d, tcif.data_sys(read)=8'h%2h",
-                     $time, i, tcif.data_sys);
+            $display("%5dns: Reading: address=%0d, data_sys(read)=8'h%2h",
+                     $time, i, tcif.memcb.data_sys);
 
             ++nReads;
 
-            tcif.addr_sys      = 0;
-            tcif.cmd_valid_sys = 0;
+            tcif.memcb.addr_sys      = 0;
+            tcif.memcb.cmd_valid_sys = 0;
         end
 
         // Wait for the bus to be idle for 100 cycles. This is an indication
@@ -71,7 +71,7 @@ program testcase(interface tcif);
         while(!done) begin
             @(posedge tcif.clk);
 
-            if (tcif.addr_sys == 0)
+            if (tcif.memcb.addr_sys == 0)
                 idleCount++;
 
             done = (idleCount >= 100);
