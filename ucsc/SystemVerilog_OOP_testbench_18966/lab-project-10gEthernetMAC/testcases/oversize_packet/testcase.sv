6bfb816d4a90088fba5d61f586867fd5d3762d31
John Hubbard <jhubbard@nvidia.com>
2015-08-17 01:22:58 -0700

    Added common runsim for all testcases. Added skeleton other testcases that all pass. run-all-test.sh now works

---
+++ ucsc/SystemVerilog_OOP_testbench_18966/lab-project-10gEthernetMAC/testcases/oversize_packet/testcase.sv
@@ -0,0 +1,23 @@
+// loopback/testcase.sv:
+
+program testcase(interface tcif_driver,
+                 interface tcif_monitor);
+
+    env env0;
+    int num_packets;
+
+    initial begin
+
+        // Enable waveform dumps for use by Synopsys DVE:
+        $vcdpluson();
+
+        env0 = new(tcif_driver, tcif_monitor);
+
+        num_packets = $urandom_range(4, 10);
+        env0.run(num_packets);
+
+        #100 $finish;
+    end
+
+endprogram
+
