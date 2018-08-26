ad36248fe67a5c9c737174bb2bf28f62b9488fa4
John Hubbard <jhubbard@nvidia.com>
2018-08-25 17:49:38 -0700

    This is an example for Kelly to see how git works

--- ucsc/verilog_0764/hw1/two_2_1_mux.v
+++ ucsc/verilog_0764/hw1/two_2_1_mux.v
@@ -1,9 +1,13 @@
 // File: two_2_1_mux.v
 // John Hubbard, 25 Sep 2014
+//
+// This change is for Kelly.
 // hw1 assignment
 
-module two_2_1_mux(a,b,select,f);
-    input a, b, select;
+module two_2_1_mux(a,b,cselect,f);
+    input a, b, c, select;
     output f;
-    assign f = (select & a) | ((~select) & b);
+
+    // New c variable for kelly
+    assign f = (select & a) | ((~select) & b) | c;
 endmodule
